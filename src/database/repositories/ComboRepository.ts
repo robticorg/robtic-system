ace16f7d92369d562ed0f7aa6e8a00252e72d3e3
robo38 <fa83734@gmail.com>
2026-07-15 23:00:56 +0100

    fix: mongoose not update

--- src/database/repositories/ComboRepository.ts
+++ src/database/repositories/ComboRepository.ts
@@ -83,7 +83,7 @@ export class ComboRepository {
                     },
                 },
             ],
-            { returnDocument: "after" }
+            { returnDocument: "after", updatePipeline: true }
         );
     }
 
